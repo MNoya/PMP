0ce8bd2fd934ad63395c815003751cba9ddf492e
MNoya <martinnoya@gmail.com>
2015-10-02 22:44:07 -0300

    Flame Strike now destroys barricades

--- game/dota_addons/pmp/scripts/vscripts/abilities/blood_elf/flame_strike.lua
+++ game/dota_addons/pmp/scripts/vscripts/abilities/blood_elf/flame_strike.lua
@@ -8,4 +8,15 @@ function FlameStrikeAnimation2( event )
     local caster = event.caster
 
     StartAnimation(caster, {duration=1, activity=ACT_DOTA_CAST_SUN_STRIKE, rate=1})
+    local point = event.target_points[1]
+
+    -- Destroy barricades
+    local units = FindUnitsInRadius(caster:GetTeamNumber(), point, nil, 200, DOTA_UNIT_TARGET_TEAM_BOTH, DOTA_UNIT_TARGET_ALL, 0, 0, false)
+
+    for k,v in pairs(units) do
+        if v:GetUnitName() == "barricade" then
+            v:ForceKill(true)
+            ParticleManager:CreateParticle("particles/newplayer_fx/npx_tree_break.vpcf", PATTACH_ABSORIGIN, v)
+        end
+    end
 end
