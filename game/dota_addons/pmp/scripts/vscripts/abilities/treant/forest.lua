28d766e4bb50181cac83f1c36553be3c1530820d
MNoya <martinnoya@gmail.com>
2015-10-09 17:55:19 -0200

    Treant racial almost done

--- game/dota_addons/pmp/scripts/vscripts/abilities/treant/forest.lua
+++ game/dota_addons/pmp/scripts/vscripts/abilities/treant/forest.lua
@@ -1,19 +1,9 @@
-MAX_FOREST = 10
-
-
 --[[
-
-    particles/econ/courier/courier_trail_fungal/courier_trail_fungal.vpcf
-    particles/econ/courier/courier_greevil_green/courier_greevil_green_ambient_3.vpcf
-    particles/econ/courier/courier_shagbark/courier_shagbark_butterflies.vpcf
-    particles/units/heroes/hero_treant/treant_overgrowth_vine_glow_trail.vpcf
-    particles/units/heroes/hero_treant/treant_overgrowth_vines.vpcf
-    particles/units/heroes/hero_treant/treant_overgrowth_cast_tree.vpcf
-    particles/units/heroes/hero_treant/treant_overgrowth_ambient_beam_sparkly.vpcf
-    particles/units/heroes/hero_treant/treant_leech_seed.vpcf
-    particles/units/heroes/hero_treant/treant_eyesintheforest.vpcf
-
-    particles/units/heroes/hero_treant/treant_leech_seed_rope.vpcf
+    particles/custom/treant/forest.vpcf
+    particles/custom/treant/green.vpcf  
+    particles/custom/treant/treant_leech_seed.vpcf
+    particles/custom/treant/overgrowth_cast.vpcf
+    particles/custom/treant/link.vpcf
 ]]
 
 function LinkStart( event )
@@ -24,61 +14,149 @@ function LinkStart( event )
     if target:GetUnitName() == "treant" then
         local link_time = ability:GetSpecialValueFor("link_time")
         ability:ApplyDataDrivenModifier(caster, target, "modifier_treant_linking", {duration=link_time})
-        print("Link Started")
+        --print("Link Started")
+
+        --StartAnimation(target, {duration=2, activity=treant_animations[1], rate=0.5})
     end
 end
 
 function LinkThink( event )
     local target = event.target
-    local ability = event.ability
-    local radius = ability:GetCastRange()
+    local radius = 300
 
     local resultGroup = GetTreantGroup(target, radius)
     local treantCount = resultGroup.unitCount
 
+    local linkTree = FindClosestTreantToLink(target, radius)
+
+    if linkTree and target.linkTarget and target.linkTarget ~= linkTree then
+        CreateParticleLink( target, linkTree )
+    end
+
+    local max_stacks = 10
+    if treantCount > max_stacks then treantCount = max_stacks end
+
     SetForestStacks(target, treantCount)
+end
+
+function LinkingCheck( event )
+    local target = event.target
+    local radius = 300
+
+    local treeOrigin = FindClosestTreant(target, radius)
+
+    if treeOrigin then
+        target.skipLink = nil
+        local particleName = "particles/custom/treant/treant_leech_seed.vpcf"
+        local leech = ParticleManager:CreateParticle(particleName, PATTACH_ABSORIGIN_FOLLOW, treeOrigin)
+        ParticleManager:SetParticleControlEnt(leech, 0, treeOrigin, PATTACH_POINT_FOLLOW, "attach_hitloc", treeOrigin:GetAbsOrigin(), true)
+        ParticleManager:SetParticleControlEnt(leech, 1, target, PATTACH_POINT_FOLLOW, "attach_hitloc", target:GetAbsOrigin(), true)
+
+        local modifier = target:FindModifierByName("modifier_treant_linking")
+        if modifier then
+            local elapsedTime = modifier:GetElapsedTime()
+            if elapsedTime >= 5 then
+                StartAnimation(target, {duration=2, activity=treant_animations[4], rate=1})
+                Timers:CreateTimer(1, function()
+                    local particleTree = "particles/custom/treant/overgrowth_cast.vpcf"
+                    ParticleManager:CreateParticle(particleTree, PATTACH_ABSORIGIN_FOLLOW, target)
+                end)
+            elseif elapsedTime >= 3 then
+                StartAnimation(target, {duration=2, activity=treant_animations[3], rate=0.5})
+            else
+                StartAnimation(target, {duration=2, activity=treant_animations[2], rate=0.5})
+            end            
+        end
+    end
+end
+
+treant_animations = {
+    [1] = ACT_DOTA_CAST_ABILITY_1,
+    [2] = ACT_DOTA_CAST_ABILITY_3,
+    [3] = ACT_DOTA_CAST_ABILITY_4,
+    [4] = ACT_DOTA_CAST_ABILITY_5
+}
+
+function StopLinking( event )
+    local target = event.unit
+    target:RemoveModifierByName("modifier_treant_linking")
+    target:RemoveModifierByName("modifier_treant_proximity")
+    target.skipLink = true
 end
 
 function LinkStablished( event )
     local caster = event.caster
     local target = event.target
+    if target.skipLink then 
+        target.skipLink = nil
+        return
+    end
     local ability = event.ability
     local radius = ability:GetCastRange()
 
-    -- Check if still in range of a treant
-    local resultGroup = GetTreantGroup(target, radius)
-    local nearbyTreants = resultGroup.unitList
-    local treantCount = resultGroup.unitCount
+    Timers:CreateTimer(function() 
+        -- Check if still in range of a treant
+        local resultGroup = GetTreantGroup(target, radius)
+        local nearbyTreants = resultGroup.unitList
+        local treantCount = resultGroup.unitCount
 
-    if treantCount > 0 then
-        print("Link Stablished")
-        local modifierName = "modifier_forest_link"
+        --print("Treant Count for",target:GetEntityIndex(),"Is",treantCount)
 
-        if treantCount > MAX_FOREST then treantCount = MAX_FOREST end
+        if treantCount > 0 then
+            --print("Link Stablished")
 
-        for k,v in pairs(nearbyTreants) do
-            SetForestStacks(v, treantCount)
+            local modifierName = "modifier_forest_link"
+            local max_stacks = 10
+
+            if treantCount > max_stacks then treantCount = max_stacks end
+
+            for k,v in pairs(nearbyTreants) do
+                SetForestStacks(v, treantCount)
+            end
+
+            CreateParticleLink(target, caster)
         end
+    end)
+end
 
-        -- Add tether link on the new linked treant
-        local particleName = "particles/units/heroes/hero_wisp/wisp_tether.vpcf"
-        target.particleLink = ParticleManager:CreateParticle(particleName, PATTACH_ABSORIGIN_FOLLOW, caster)
-        ParticleManager:SetParticleControlEnt(target.particleLink, 0, caster, PATTACH_POINT_FOLLOW, "attach_hitloc", caster:GetAbsOrigin(), true)
-        ParticleManager:SetParticleControlEnt(target.particleLink, 1, target, PATTACH_POINT_FOLLOW, "attach_hitloc", target:GetAbsOrigin(), true)
-    end
+function CreateParticleLink( owner, target )
+    if owner.particleLink and owner.linkTarget ~= target then
+        ParticleManager:DestroyParticle(owner.particleLink, false)
+        owner.particleLink = nil
+    end  
+
+    -- Add tether link on the new linked treant
+    -- Each linked tree has his own link to another
+    local particleName = "particles/custom/treant/link.vpcf"
+    owner.particleLink = ParticleManager:CreateParticle(particleName, PATTACH_ABSORIGIN_FOLLOW, owner)
+    ParticleManager:SetParticleControlEnt(owner.particleLink, 0, owner, PATTACH_POINT_FOLLOW, "attach_hitloc", owner:GetAbsOrigin(), true)
+    ParticleManager:SetParticleControlEnt(owner.particleLink, 1, target, PATTACH_POINT_FOLLOW, "attach_hitloc", target:GetAbsOrigin(), true)
+    owner.linkTarget = target
+
+    --print("Created particle link, owner:",owner:GetEntityIndex(),"target:",target:GetEntityIndex())
 end
 
 function BreakLink( event )
-    print("Break Link")
+    --print("Break Link")
 
     local caster = event.caster
     local target = event.target
     local radius = 300
 
     local modifierName = "modifier_forest_link"
-    local modifier = target:FindModifierByName(modifierName)
-
     target:RemoveModifierByName(modifierName)
+    target.skipLink = true
+end
+
+function CheckParticleLinks( event )
+    local caster = event.caster
+    local target = caster.linkTarget
+    if caster.particleLink and (not IsValidAlive(target) or caster:GetRangeToUnit(target) > 300) then
+        ParticleManager:DestroyParticle(caster.particleLink, false)
+        caster.particleLink = nil
+
+        --print("Destroyed particle link on caster:",caster:GetEntityIndex())
+    end
 end
 
 function SetForestStacks( unit, number )
@@ -91,6 +169,7 @@ function SetForestStacks( unit, number )
             if unit.particleLink then
                 ParticleManager:DestroyParticle(unit.particleLink, false)
                 unit.particleLink = nil
+                --print("Destroyed particle link on unit:",unit:GetEntityIndex())
             end
             unit:RemoveModifierByName(modifierName)
         else
@@ -102,6 +181,26 @@ function SetForestStacks( unit, number )
 
         modifier = unit:FindModifierByName(modifierName)
         modifier:SetStackCount(number)
+    end
+end
+
+function FindClosestTreant( unit, radius )
+    local units = FindUnitsInRadius(unit:GetTeamNumber(), unit:GetAbsOrigin(), nil, radius, DOTA_UNIT_TARGET_TEAM_FRIENDLY, DOTA_UNIT_TARGET_BASIC, DOTA_UNIT_TARGET_FLAG_NOT_MAGIC_IMMUNE_ALLIES, FIND_CLOSEST, true)
+    local treant
+    for k,v in pairs(units) do
+        if v ~= unit and v:GetUnitName() == "treant" then
+            return v
+        end
+    end
+end
+
+function FindClosestTreantToLink( unit, radius )
+    local units = FindUnitsInRadius(unit:GetTeamNumber(), unit:GetAbsOrigin(), nil, radius, DOTA_UNIT_TARGET_TEAM_FRIENDLY, DOTA_UNIT_TARGET_BASIC, DOTA_UNIT_TARGET_FLAG_NOT_MAGIC_IMMUNE_ALLIES, FIND_CLOSEST, true)
+    local treant
+    for k,v in pairs(units) do
+        if v ~= unit and v:GetUnitName() == "treant" and v:HasModifier("modifier_forest_link") then
+            return v
+        end
     end
 end
 
@@ -136,9 +235,11 @@ function RecursiveFind( unit, radius, group )
         -- Add to group
         for k,v in pairs(units) do
             local index = v:GetEntityIndex()
-            if not group[index] and v:GetUnitName() == "treant" and not v:HasModifier("modifier_treant_linking") then
-                group[index] = v
-                RecursiveFind(v, radius, group)
+            if not group[index] and v:GetUnitName() == "treant" then
+                if not v:HasModifier("modifier_treant_linking") then
+                    group[index] = v
+                    RecursiveFind(v, radius, group)
+                end
             end
         end
     else
